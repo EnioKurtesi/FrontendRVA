fec128d8119fca8d79c448b16c106dfda859c19e
EnioKurtesi <enio.estiem@gmail.com>
2022-05-09 15:36:13 +0200

    Uradjene komponente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/core/about/about.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { FakultetComponent } from './components/fakultet/fakultet.component';
+import { StatusComponent } from './components/status/status.component';
+import { StudentComponent } from './components/student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'student',component: StudentComponent},
+  {path: 'status', component: StatusComponent},
+  {path: 'fakultet', component: FakultetComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'author', component: AuthorComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
