cc4d459b5c09887e630db04e40eaba7ddb16b7f5
EnioKurtesi <enio.estiem@gmail.com>
2022-06-17 10:14:34 +0200

    Poslednji commit frontend

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const STATUS_URL = 'http://localhost:8083/status';
-export const FAKULTET_URL = 'http://localhost:8083/fakultet';
-export const DEPARTMAN_URL = 'http://localhost:8083/departman';
-export const STUDENT_ZA_DEPARTMAN_URL = 'http://localhost:8083/studentByDepartman'
-export const STUDENT_URL = 'http://localhost:8083/student';
+export const STATUS_URL = 'https://backend-rva-it27-2019.herokuapp.com/status';
+export const FAKULTET_URL = 'https://backend-rva-it27-2019.herokuapp.com/fakultet';
+export const DEPARTMAN_URL = 'https://backend-rva-it27-2019.herokuapp.com/departman';
+export const STUDENT_ZA_DEPARTMAN_URL = 'https://backend-rva-it27-2019.herokuapp.com/studentByDepartman'
+export const STUDENT_URL = 'https://backend-rva-it27-2019.herokuapp.com/student';
